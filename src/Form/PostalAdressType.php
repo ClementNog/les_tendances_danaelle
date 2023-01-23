b3034f8df003012c2a822ad8d0ca20dcdb628eed
clement nogueire <Sioslam72@gmail.com>
2023-01-23 12:15:39 +0100

    firstcommit

---
+++ src/Form/PostalAdressType.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+class PostalAddressType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('address', TextType::class, [
+                'help' => 'Street address, P.O. box, company name',
+            ])
+            ->add('addressLine2', TextType::class, [
+                'help' => 'Apartment, suite, unit, building, floor',
+            ])
+            ->add('city', TextType::class)
+            ->add('zipCode', TextType::class, [
+                'label' => 'ZIP Code',
+            ])
+        ;
+    }
+}
+?>
